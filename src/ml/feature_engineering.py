9cbcc5d1209d34e8a56f91d94f1b266b27b68ab0
Larbi Asmaoui <103370123+larbi-asmaoui@users.noreply.github.com>
2026-01-10 12:54:56 +0100

    Update feature_engineering.py

--- src/ml/feature_engineering.py
+++ src/ml/feature_engineering.py
@@ -4,17 +4,17 @@ from pyspark.sql.window import Window
 
 def transform_data(df: DataFrame) -> DataFrame:
     """
-    Features:
-    - Log Returns: log(avg_price / prev_avg_price)
-    - Volatility: Passed from Gold (filled with 0.0 if null)
-    - Log Volume: log(total_volume + 1)
+    Calculates features for Random Forest.
     """
     w = Window.partitionBy("symbol").orderBy("window_start")
     
-    df_feat = df \
+    # 1. Calculate Technical Indicators
+    df = df \
         .withColumn("prev_price", lag("avg_price", 1).over(w)) \
         .withColumn("log_returns", log(col("avg_price") / col("prev_price"))) \
         .withColumn("log_volume", log(col("total_volume") + 1.0)) \
         .withColumn("volatility", coalesce(col("volatility"), lit(0.0)))
         
-    return df_feat.na.drop(subset=["log_returns", "log_volume", "avg_price"])
+    # 2. Cleanup
+    df = df.na.drop(subset=["log_returns", "log_volume", "avg_price"])
+    return df
